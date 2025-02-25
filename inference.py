dd8832a2d2eb9e0b1da37e3432ff4304004283b5
Altintas <reyaah@rambler.ru>
2025-02-25 18:08:38 +0500

    init bot

---
+++ inference.py
@@ -0,0 +1,103 @@
+from keras.models import Model
+from keras.layers import Input
+import numpy as np
+import model as m
+import chatbot_data as dp
+
+
+enc_model = Model([m.enc_inp], m.enc_states)
+
+# decoder Model
+decoder_state_input_h = Input(shape=(400,))
+decoder_state_input_c = Input(shape=(400,))
+
+decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+
+decoder_outputs, state_h, state_c = m.dec_lstm(m.dec_embed , 
+                                    initial_state=decoder_states_inputs)
+
+
+decoder_states = [state_h, state_c]
+
+
+dec_model = Model([m.dec_inp]+ decoder_states_inputs,
+                                      [decoder_outputs]+ decoder_states)
+
+
+
+
+from keras.preprocessing.sequence import pad_sequences
+print("##########################################")
+print("#       start chatting ver. 1.0          #")
+print("##########################################")
+
+
+prepro1 = ""
+while prepro1 != 'q':
+    prepro1  = input("you : ")
+    ## prepro1 = "Hello"
+
+    prepro1 = dp.clean_text(prepro1)
+    ## prepro1 = "hello"
+
+    prepro = [prepro1]
+    ## prepro1 = ["hello"]
+
+    txt = []
+    for x in prepro:
+        # x = "hello"
+        lst = []
+        for y in x.split():
+            ## y = "hello"
+            try:
+                lst.append(dp.vocab[y])
+                ## vocab['hello'] = 454
+            except:
+                lst.append(dp.vocab['<OUT>'])
+        txt.append(lst)
+
+    ## txt = [[454]]
+    txt = pad_sequences(txt, 13, padding='post')
+
+    ## txt = [[454,0,0,0,.........13]]
+
+    stat = enc_model.predict( txt )
+
+    empty_target_seq = np.zeros( ( 1 , 1) )
+     ##   empty_target_seq = [0]
+
+
+    empty_target_seq[0, 0] = dp.vocab['<SOS>']
+    ##    empty_target_seq = [255]
+
+    stop_condition = False
+    decoded_translation = ''
+
+    while not stop_condition :
+
+        dec_outputs , h, c= dec_model.predict([ empty_target_seq] + stat )
+        decoder_concat_input = m.dense(dec_outputs)
+        ## decoder_concat_input = [0.1, 0.2, .4, .0, ...............]
+
+        sampled_word_index = np.argmax( decoder_concat_input[0, -1, :] )
+        ## sampled_word_index = [2]
+
+        sampled_word = dp.inv_vocab[sampled_word_index] + ' '
+
+        ## inv_vocab[2] = 'hi'
+        ## sampled_word = 'hi '
+
+        if sampled_word != '<EOS> ':
+            decoded_translation += sampled_word  
+
+        if sampled_word == '<EOS> ' or len(decoded_translation.split()) > 13:
+            stop_condition = True 
+
+        empty_target_seq = np.zeros( ( 1 , 1 ) )  
+        empty_target_seq[ 0 , 0 ] = sampled_word_index
+        ## <SOS> - > hi
+        ## hi --> <EOS>
+        stat = [h, c]  
+
+    print("chatbot attention : ", decoded_translation )
+    print("==============================================")  
